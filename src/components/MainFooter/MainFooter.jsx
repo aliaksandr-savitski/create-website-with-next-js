1cf2520985e5a2164baacdd09a5109edf031915d
aliaksandr-savitski <alieks.sawicki@gmail.com>
2020-07-26 19:08:50 +0300

    chore: footer cleaning

--- src/components/MainFooter/MainFooter.jsx
+++ src/components/MainFooter/MainFooter.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-import { media } from '@styles/style-utils';
-
-const FooterStyledComponent = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: red;
-
-  ${media.tablet`
-    background: green;
-  `}
-`;
 
 const MainFooter = () => (
-  <div>
-    Main Footer
-
-    <FooterStyledComponent />
-  </div>
+  <div>Main Footer</div>
 );
 
 export default MainFooter;
